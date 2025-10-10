422208453b48a55345232eff02de4f357a90e33f
Lakshit Agarwal <lakshit@hivel.ai>
2025-10-10 17:57:52 +0530

    implemented redux

--- src/components/mainContainer/userCards/UserCards.tsx
+++ src/components/mainContainer/userCards/UserCards.tsx
@@ -5,11 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { Modal } from "../../Modal/modal";
 import type { UserType } from "../../../Utils/userInterface";
-
-const fetchUsers = async () => {
-  const res = await axios.get("https://dummyjson.com/users");
-  return res.data.users;
-};
+import { useDispatch } from "react-redux";
+import { setUsers } from "../../../Utils/features/userSlice";
 
 export function UserCards({
   searchQuery,
@@ -18,16 +15,21 @@ export function UserCards({
   searchQuery: string;
   filterQuery: string[];
 }) {
+  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState<boolean>(false);
   const [selectedUser, setSelectedUser] = useState<UserType>();
   const { data: users, isLoading } = useQuery({
     queryKey: ["users"],
-    queryFn: fetchUsers,
+    queryFn: async () => {
+      const res = await axios.get("https://dummyjson.com/users");
+      dispatch(setUsers(res.data.users));
+      return res.data.users;
+    },
   });
 
   if (isLoading) return <div>Loading...</div>;
 
-  const searchUsers = users.filter(
+  const searchUsers = users?.filter(
     (user: UserType) =>
       user.firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
       user.lastName.toLowerCase().includes(searchQuery.toLowerCase())
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+import {store} from "./Utils/store/store.ts";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </div>
 );
